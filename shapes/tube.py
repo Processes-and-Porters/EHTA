8c4ea29e99b537388d1c1580deec2766f7f8af0c
wantiashu <wantiashu@126.com>
2020-12-07 22:31:08 +0800

    2020.12.7

--- shapes/tube.py
+++ shapes/tube.py
@@ -1,18 +1,25 @@
 # tube is a shape 
 class tube
 
-   diameter = ""
+   inner_diameter = ""
+   outer_diameter = ""
+   wall_thickness = ""
    length = ""
    heat_transfer_area = ""
    cross_section_area = ""
 
-   def _init_(self,diameter,length):
-       self.diameter = diameter
+   def _init_(self,outer_diameter,wall_thickness,length):
+       self.outer_diameter = outer_diameter
+       self.wall_thickness = wall_thickness
+       self.inner_diameter = inner_diameter
        self.length = length
 
-   def heat_transfer_area():       
-       return 3.14*diameter*length
+   def outer_heat_transfer_area():       
+       return 3.14*outer_diameter*length
 
-   def cross_section_area():       
-       return 3.14*diameter^2/4
+   def inner_heat_transfer_area():       
+       return 3.14*inner_diameter*length
+
+   def flow_cross_section_area():       
+       return 3.14*inner_diameter^2/4
 
